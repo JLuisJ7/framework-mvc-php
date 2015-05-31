36be2fc0f4bb11a503b0f7b5a4ba3019d7ac1179
jluisj7 <jluisab7@gmail.com>
2015-05-31 16:30:20 -0500

    incluyendo archivos de la aplicacion y definiendo las constantes

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo $_GET['url'];
+//definiendo las constantes
+define('DS', DIRECTORY_SEPARATOR); //separador /
+define('ROOT', realpath(dirname(__FILE__)).DS);// Ruta base de la aplicacion
+//echo ROOT;
+
+define('APP_PATH',ROOT . 'application'.DS);
+
+require_once APP_PATH.'Config.php';
+require_once APP_PATH.'Request.php';//va a recibir las peticiones por la url y las va a enviar a Bootstrap
+require_once APP_PATH.'Bootstrap.php';//Bootstrap va a  llamar al controllers
+require_once APP_PATH.'Controller.php';//controlador principal 
+require_once APP_PATH.'Model.php';//metodos comunes para todos models
+require_once APP_PATH.'View.php';
+require_once APP_PATH.'Registro.php';
+
+
+//echo '<pre>';print_r(get_required_files()); //
+
+
 ?>
